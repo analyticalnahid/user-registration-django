7aba0d3d58a568cc73f651d4b0c7e0f58367a10a
Nahidul Islam <97219701+analyticalnahid@users.noreply.github.com>
2023-01-24 14:51:03 +0600

    Add files via upload

---
+++ usermodel_reg/usermodel_reg_app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from usermodel_reg_app.models import UserProfileInfo
+
+admin.site.register(UserProfileInfo)
---
+++ usermodel_reg/usermodel_reg_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UsermodelRegAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'usermodel_reg_app'
